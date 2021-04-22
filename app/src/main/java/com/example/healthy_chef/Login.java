5f051ef1a20a44db96bee0f90af133a9c9720980
Anderson <andersontan1998@yahoo.com>
2021-04-22 01:02:46 -0400

    imported firebase auth for login and worked on register/login

---
+++ app/src/main/java/com/example/healthy_chef/Login.java
@@ -0,0 +1,111 @@
+package com.example.healthy_chef;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    // Define class variables from the page
+    private Button b_login;
+    private EditText et_email, et_password;
+    private TextView register;
+    // firebase object
+    private FirebaseAuth firebaseAuth;
+
+    // on creation of activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // set class variables equal to their inputs
+        et_email = (EditText) findViewById(R.id.et_login_email);
+        et_password = (EditText) findViewById(R.id.et_login_password);
+        b_login = (Button) findViewById(R.id.b_login);
+        register = (TextView) findViewById(R.id.tv_login_register);
+
+        // get instance of firebase authentication
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // listen to which is going to be clicked
+        b_login.setOnClickListener(this);
+        register.setOnClickListener(this);
+    }
+
+    // Check which listener was called
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // user pressed login button
+            case R.id.b_login:
+                // if inputs are valid, sign in the user
+                if (validate()) {
+                    signIn();
+                }
+                break;
+            // user pressed on register button, send to register activity
+            case R.id.tv_login_register:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
+    }
+
+    /* simple validation function, checks if inputs are empty
+    returns true if inputs are correct format
+    note: add better check, display which field is missing
+     */
+    private boolean validate() {
+        boolean result = false;
+
+        String email = et_email.getText().toString();
+        String password = et_password.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "You're forgetting a field!", Toast.LENGTH_SHORT).show();
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+
+    /* function that signs in user using firebase authentication
+    Does not return anything, sends user to next activity
+     */
+    private void signIn() {
+        // convert inputs to strings, final because passing it to homePage
+        final String email = et_email.getText().toString();
+        String password = et_password.getText().toString();
+        // firebase authentication
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener
+                (new OnCompleteListener<AuthResult>() {
+                    // when firebase authentication is completed
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // user was able to login, move him to home page activity
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, HomePage.class)
+                                    .putExtra("email", email));
+                        } // could not login, display error
+                        else if (!task.isSuccessful()) {
+                            String errorCode = ((FirebaseAuthException) task.getException()).getMessage();
+                            Toast.makeText(Login.this, "Login failed! " + errorCode, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/healthy_chef/Register.java
@@ -0,0 +1,142 @@
+package com.example.healthy_chef;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    // Define class variables from the page
+    private Button b_register;
+    private EditText et_email, et_name, et_password;
+    // firebase/debugging variables
+    private static final String TAG = "Register.java";
+    private static final String name = "name";
+    private static final String password = "password";
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+
+    // on creation of activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // set class variables equal to their inputs
+        et_email = (EditText) findViewById(R.id.et_register_email);
+        et_name = (EditText) findViewById(R.id.et_register_name);
+        et_password = (EditText) findViewById(R.id.et_register_password);
+        b_register = (Button) findViewById(R.id.b_register);
+
+        // get current activity instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // listen if the user has clicked the register button
+        b_register.setOnClickListener(this);
+    }
+
+    // Check which listener was called
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            // register button was clicked
+            case R.id.b_register:
+                // validated, upload data to the database
+                if (validate()) {
+                    final String email = et_email.getText().toString();
+                    String password = et_password.getText().toString();
+                    // create user and upload to authentication database
+                    firebaseAuth.createUserWithEmailAndPassword(
+                            email,
+                            password
+                    ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        // on completion of sending to firebase
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            // if account was made
+                            if (task.isSuccessful()) {
+                                // save user in database
+                                saveUser();
+                                Toast.makeText(Register.this, "Your account was made!",
+                                        Toast.LENGTH_SHORT).show();
+                                // include email value for next activity
+                                startActivity(new Intent(Register.this, RegistrationInfo.class)
+                                        .putExtra("email_from_reg", email));
+                            } // display which error using errCode
+                            else if (!task.isSuccessful()){
+                                String errorCode = ((FirebaseAuthException) task.getException()).getMessage();
+                                Toast.makeText(Register.this, "Registration failed: " + errorCode,
+                                        Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+                }
+                break;
+        }
+    }
+
+    /* simple validation function for register inputs
+    returns true if no inputs were empty
+    note: include better checks? display which field is missing
+     */
+    private boolean validate() {
+        Boolean result = false;
+
+        String name = et_name.getText().toString();
+        String email = et_email.getText().toString();
+        String password = et_password.getText().toString();
+
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "You're forgetting a field!", Toast.LENGTH_SHORT).show();
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+
+    // save user to database
+    public void saveUser() {
+        String email = et_email.getText().toString();
+        String user_password = et_password.getText().toString();
+        String user_name = et_name.getText().toString();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put(name, user_name);
+        user.put(password, user_password);
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("users").document(email).collection("activities")
+                .document("user_credentials").set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+}
